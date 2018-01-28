34b8866e439c24b644eea35f2f414ab65072858d
yatei <scherphy@gmail.com>
2018-01-29 01:19:50 +0900

    auto commit

--- keymap.c
+++ keymap.c
@@ -4,10 +4,6 @@
 
 extern keymap_config_t keymap_config;
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
 #define _QWERTY 0
 #define _LOWER 1
 
@@ -16,12 +12,9 @@ enum custom_keycodes {
   LOWER,
 };
 
-// Fillers to make layering more clear
-#define KC_ KC_TRNS
 #define _______ KC_TRNS
-#define XXXXXXX KC_NO
-#define KC_LOWR LOWER
 #define XXX KC_NO
+#define KC_LOWR LOWER
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -33,17 +26,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  | Del  |      |   '  |   H  |   J  |   K  |   L  |   ;  | Enter|
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |      |      |   \  |   N  |   M  |   ,  |   .  |   /  |  Up  |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |      |   \  |   N  |   M  |   ,  |   .  |   /  |  Up  |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * | Esc  | GUI  | Alt  | Lower| Bksp | Space| Enter|      | Bksp | Space| GUI  |      | Left | Right| Down |
+ * | Esc  | GUI  | Alt  | Lower| Bksp | Space| Enter|      | Bksp | Space| GUI  | Alt  | Left | Right| Down |
  * `------------------------------------------------'      `------------------------------------------------'
  */
   [_QWERTY] = KEYMAP(
 KC_GRV ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5  ,KC_6   , KC_7   ,KC_8   ,KC_9  ,KC_0   ,KC_MINS,KC_EQL ,KC_BSPC,
 KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T  ,KC_BSPC, KC_RBRC,KC_Y  ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_LBRC,
 KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G  ,KC_DEL , KC_QUOT,KC_H  ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_ENT ,
-KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B  ,XXX    , KC_BSLS,KC_N  ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_UP  ,
-KC_ESC,KC_LGUI,KC_LALT ,LOWER  ,KC_BSPC,KC_SPC,KC_ENT , KC_BSPC,KC_SPC,KC_RGUI,XXX    ,KC_LEFT,KC_RGHT,KC_DOWN
+KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B  ,KC_N   , KC_BSLS,KC_N  ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_UP  ,
+KC_ESC,KC_LGUI,KC_LALT ,LOWER  ,KC_BSPC,KC_SPC,KC_ENT , KC_BSPC,KC_SPC,KC_RGUI,KC_RALT,KC_LEFT,KC_RGHT,KC_DOWN
   ),
 
 /* LOWER = Top: Fn Keys, Left: Mouse and Media, Right(Rotate 90°): Numpad
