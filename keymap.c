89244acb58c52f6585491595e47b6249e09de0c2
yatei <scherphy@gmail.com>
2018-01-30 04:40:22 +0900

    auto commit

--- keymap.c
+++ keymap.c
@@ -24,19 +24,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  | Bksp |      |   ]  |   Y  |   U  |   I  |   O  |   P  |   [  |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  | Del  |      |   '  |   H  |   J  |   K  |   L  |   ;  | Enter|
+ * | Ctrl |   A  |   S  |   D  |   F  |   G  | Del  |      | Ctrl |   H  |   J  |   K  |   L  |   ;  |   '  |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |      |   \  |   N  |   M  |   ,  |   .  |   /  |  Up  |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |      | Shift|   N  |   M  |   ,  |   .  |   /  |   \  |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * | Esc  | GUI  | Alt  | Lower| Bksp | Space| Enter|      | Bksp | Space| GUI  | Alt  | Left | Right| Down |
+ * | Esc  | GUI  | Alt  | Lower| Bksp | Space| Enter|      | Enter| Space| GUI  | Left |  Up  | Down | Right|
  * `------------------------------------------------'      `------------------------------------------------'
  */
   [_QWERTY] = KEYMAP(
 KC_GRV ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5  ,KC_6   , KC_7   ,KC_8   ,KC_9  ,KC_0   ,KC_MINS,KC_EQL ,KC_BSPC,
 KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T  ,KC_BSPC, KC_RBRC,KC_Y  ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_LBRC,
-KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G  ,KC_DEL , KC_QUOT,KC_H  ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_ENT ,
-KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B  ,KC_N   , KC_BSLS,KC_N  ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_UP  ,
-KC_ESC,KC_LGUI,KC_LALT ,LOWER  ,KC_BSPC,KC_SPC,KC_ENT , KC_BSPC,KC_SPC,KC_RGUI,KC_RALT,KC_LEFT,KC_RGHT,KC_DOWN
+KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G  ,KC_DEL , KC_RCTL,KC_H  ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT,
+KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B  ,KC_N   , KC_RSFT,KC_N  ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLS,
+KC_ESC,KC_LGUI,KC_LALT ,LOWER  ,KC_BSPC,KC_SPC,KC_ENT , KC_ENT ,KC_SPC,KC_RGUI,KC_LEFT,KC_UP  ,KC_DOWN,KC_RIGHT
   ),
 
 /* LOWER = Top: Fn Keys, Left: Mouse and Media, Right(Rotate 90°): Numpad
@@ -49,7 +49,7 @@ KC_ESC,KC_LGUI,KC_LALT ,LOWER  ,KC_BSPC,KC_SPC,KC_ENT , KC_BSPC,KC_SPC,KC_RGUI,K
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
  * | Shift|↓Wheel|Speed0|Speed1|Speed2|↓Wheel| Mute |      |      |      |   /  |   8  |   5  |   2  |   0  |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |  Esc |  GUI |  Alt | Lower|      |PrvMsc|NxtMsc|      |      |      |NumLck|   7  |   4  |   1  |   0  |
+ * |  Esc |  GUI |  Alt | Lower| Space|PrvMsc|NxtMsc|      |      |      |NumLck|   7  |   4  |   1  |   0  |
  * `------------------------------------------------'      `------------------------------------------------'
  */
   [_LOWER] = KEYMAP(
